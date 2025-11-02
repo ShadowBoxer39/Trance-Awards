26ee857223e948ca269e7db61dd4d37575c22733
ShadowBoxer39 <assiftal@gmail.com>
2025-11-02 20:40:44 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 // pages/_app.tsx
 import type { AppProps } from "next/app";
-import "../styles/globals.css"; // <-- IMPORTANT (use this relative path)
+import "../styles/globals.css";
+import { gan, milky } from "../lib/fonts";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${gan.variable} ${milky.variable}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
