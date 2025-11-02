b8635e702b360bcee8e9e26c384811b90821b993
ShadowBoxer39 <assiftal@gmail.com>
2025-11-02 16:40:47 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
+// pages/_app.tsx
 import type { AppProps } from "next/app";
-import "../styles/globals.css";
-export default function App({ Component, pageProps }: AppProps) {
+import "../styles/globals.css"; // <-- IMPORTANT (use this relative path)
+
+export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
