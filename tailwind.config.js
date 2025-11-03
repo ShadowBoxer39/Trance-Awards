3c4686efbbf77dfc15437abf30ce34ff7876917b
ShadowBoxer39 <assiftal@gmail.com>
2025-11-03 11:30:47 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-  extend: {
-  fontFamily: {
-    gan: ["var(--font-gan)", "sans-serif"],
-    milkyway: ["var(--font-milkyway)", "sans-serif"],
+    extend: {
+      fontFamily: {
+        gan: ["var(--font-gan)", "sans-serif"],
+        milkyway: ["var(--font-milkyway)", "sans-serif"],
+      },
+    },
   },
-},
   plugins: [],
 };
