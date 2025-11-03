9621260aa68d04edd74726febbd12054030c1615
ShadowBoxer39 <assiftal@gmail.com>
2025-11-03 11:15:37 +0200

    Update fonts.ts

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,9 +1,13 @@
-// lib/fonts.ts
 import localFont from "next/font/local";
 
-// Note: fonts.ts sits in /lib, so the .ttf is at ./fonts/...
 export const milkyway = localFont({
   src: [{ path: "./fonts/Milkyway DEMO.ttf", weight: "400", style: "normal" }],
   display: "swap",
   variable: "--font-milkyway",
+});
+
+export const gan = localFont({
+  src: [{ path: "./fonts/Gan CLM Bold.ttf", weight: "700", style: "normal" }],
+  display: "swap",
+  variable: "--font-gan",
 });
