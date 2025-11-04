b672d3018ff05f89fb799aa58c55429b02f10f0d
ShadowBoxer39 <assiftal@gmail.com>
2025-11-04 15:22:54 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
-// pages/_app.tsx
+// /pages/_app.tsx
 import "../styles/globals.css";
 import "../styles/theme.css";
 
 import type { AppProps } from "next/app";
 import { milkyway, gan } from "../lib/fonts";
+import PlayerProvider from "../components/PlayerProvider"; // ← add this
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${milkyway.variable} ${gan.variable} font-gan`}>
-      <Component {...pageProps} />
+      <PlayerProvider>
+        <Component {...pageProps} />
+      </PlayerProvider>
     </main>
   );
 }
