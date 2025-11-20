6a0776a8f3cb67616f10bd42fa0dfbe8e26237e4
ShadowBoxer39 <assiftal@gmail.com>
2025-11-20 11:51:33 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -411,7 +411,14 @@ export default function Home() {
                 <div>
                   <h4 className="text-lg font-semibold mb-3">הטרק המוצג</h4>
                   <div className="rounded-lg overflow-hidden">
-                   <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2202896507&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/kanok_music" title="Kanok" target="_blank" style="color: #cccccc; text-decoration: none;">Kanok</a> · <a href="https://soundcloud.com/kanok_music/kanok-light-beam" title="Kanok - Light Beam" target="_blank" style="color: #cccccc; text-decoration: none;">Kanok - Light Beam</a></div>
+<iframe
+  width="100%"
+  height="300"
+  scrolling="no"
+  style={{ border: 'none' }}
+  allow="autoplay"
+  src="https://w.soundcloud.com/player/?url=https%3A//soundcloud.com/kanok_music/kanok-light-beam"
+></iframe>                  
                   </div>
                 </div>
 
