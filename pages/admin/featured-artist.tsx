5a2a8816e3def2e284c96fb9bd26fe77c8a08f1d
ShadowBoxer39 <assiftal@gmail.com>
2025-11-26 22:44:52 +0200

    Update featured-artist.tsx

--- pages/admin/featured-artist.tsx
+++ pages/admin/featured-artist.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { supabase } from '@/lib/supabaseClient';
+import supabase from '@/lib/supabaseServer';
 import Image from 'next/image';
 
 interface FeaturedArtist {
