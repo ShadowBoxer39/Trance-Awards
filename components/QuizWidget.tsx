4aa4301b1f48616051f054729d1efe2c360e7d2f
ShadowBoxer39 <assiftal@gmail.com>
2025-12-04 08:41:21 +0200

    Update QuizWidget.tsx

--- components/QuizWidget.tsx
+++ components/QuizWidget.tsx
@@ -4,7 +4,7 @@ import GoogleLoginButton from "./GoogleLoginButton";
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! 
 );
 
 interface QuizData {
