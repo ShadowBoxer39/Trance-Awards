b4d7de3de31983a68773c08cbf40d977fffa6c6b
ShadowBoxer39 <assiftal@gmail.com>
2025-12-07 17:02:36 +0200

    Update admin.tsx

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -5,7 +5,7 @@ import { CATEGORIES } from "@/data/awards-data";
 
 const getYouTubeVideoId = (url: string): string | null => {
   const regex = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/;
-  const match = url.match(regex);
+  const match = url.match(regex); 
   return match ? match[1] : null;
 };
 
